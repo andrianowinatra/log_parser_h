ce412ee4615ab11dc581ddfa3ca7bd6d107718c9
andriano <andriano.winatra@gmail.com>
2017-11-21 12:17:45 +0700

    add database
    add flask sqlalchemy and sqlalchemy 
    initiate db models

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from app.models import db
 
 
 def make_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
+
+    db.init_app(app)
 
     @app.route("/api/hello")
     def hello():
---
+++ app/config.py
@@ -0,0 +1,1 @@
+SQLALCHEMY_DATABASE_URI = 'sqlite://log_database.db'
