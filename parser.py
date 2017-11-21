0092560318de4c53a0ba5e27969c6666a383364a
andriano <andriano.winatra@gmail.com>
2017-11-21 16:50:33 +0700

    update parser
    parser is too slow in piping the data to db
    fix wrong user agent being pulled from the log
    fix wrong referrer being pulled from the log

--- parser.py
+++ parser.py
@@ -8,10 +8,9 @@ from app import make_app
 from app.models import db, LogEntry
 
 
-def parselogs():
-    """This is a management script for the wiki application."""
+def log_generator():
+    """ generator object for log entries and parsing """
     # Fields: date time s-ip cs-method cs-uri-stem cs-uri-query s-port cs-username c-ip cs(User-Agent) cs(Referer) sc-status sc-substatus sc-win32-status time-taken
-    current_app = make_app()
     raw_log_pattern_regex = (
         r"^(?P<date>\d{0,4}-\d{2}-\d{2})\s"
         r"(?P<time>\d{2}:\d{2}:\d{2})\s"
@@ -22,7 +21,7 @@ def parselogs():
         r"(?P<remote_port>\d{2,}|-)\s"
         r"(?P<client_username>.*|-)\s"
         r"(?P<client_ip>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\s"
-        r"(?P<ua_string>.*?|-)\s"
+        r"(?P<ua_string>.*\)|.*?|-)\s"
         r"(?P<referrer><.*|\w*.com|http.*|-)\s"
         r"(?P<request_status>\d{1,3})\s"
         r"(?P<request_sub_status>\d{1,})\s"
@@ -54,39 +53,62 @@ def parselogs():
                     time.sleep(5)
                 else:
                     temp_buffer = []
-                    with current_app.app_context():
-                        insert_log_entry(end_entry)
-
-                if 'select' in end_entry["query_params"].lower():
-                    print (end_entry)
+                    yield end_entry
 
 
-def insert_log_entry(log_entry):
-    # 2015-08-28 18:03:07
+def parselogs():
+    """This is a log parser"""
+    current_app = make_app()
+    log_gen = log_generator()
+    temp_list = []
+    for log_entry in log_gen:
+        processed_entry = process_entry(log_entry)
+        temp_list.append(processed_entry)
+        if len(temp_list) > 10000:
+            with current_app.app_context():
+                insert_log_entries(temp_list)
+            temp_list = []
+    else:
+        with current_app.app_context():
+            insert_log_entries(temp_list)
+
+
+def process_entry(log_entry):
     timestamp_format = "%Y-%m-%d %H:%M:%S"
     timestamp_raw = " ".join((log_entry["date"], log_entry["time"]))
     timestamp = datetime.datetime.strptime(timestamp_raw, timestamp_format)
-    gi = GeoIP.new(GeoIP.GEOIP_MEMORY_CACHE)
-    client_ip_country = gi.country_name_by_addr("24.24.24.24")
-    new_log_entry = LogEntry(
-        remote_host=log_entry["remote_host"],
-        remote_port=log_entry["remote_port"],
-        request_method=log_entry["request_method"],
-        requested_resources=log_entry["requested_resource"],
-        query_params=log_entry["query_params"],
-        client_username=log_entry["client_username"],
-        client_ip=log_entry["client_ip"],
-        client_ip_country=client_ip_country,
-        ua_string=log_entry["ua_string"],
-        referrer=log_entry["referrer"],
-        request_status=log_entry["request_status"],
-        request_sub_status=log_entry["request_sub_status"],
-        windows_status=log_entry["windows_status"],
-        time_taken=log_entry["time_taken"],
-        timestamp=timestamp
-    )
-    db.session.add(new_log_entry)
-    db.session.commit()
+    client_ip_country = determine_ip_origin(log_entry["client_ip"])
+
+    process_entry = {
+        "remote_host": log_entry["remote_host"],
+        "remote_port": log_entry["remote_port"],
+        "request_method": log_entry["request_method"],
+        "requested_resources": log_entry["requested_resource"],
+        "query_params": log_entry["query_params"],
+        "client_username": log_entry["client_username"],
+        "client_ip": log_entry["client_ip"],
+        "client_ip_country": client_ip_country,
+        "ua_string": log_entry["ua_string"],
+        "referrer": log_entry["referrer"],
+        "request_status": log_entry["request_status"],
+        "request_sub_status": log_entry["request_sub_status"],
+        "windows_status": log_entry["windows_status"],
+        "time_taken": log_entry["time_taken"],
+        "timestamp": timestamp
+    }
+
+    return process_entry
+
+
+def insert_log_entries(entry_list):
+    """ bulk inserts entry list to db """
+    db.engine.execute(LogEntry.__table__.insert(), entry_list)
+
+
+gi = GeoIP.new(GeoIP.GEOIP_MEMORY_CACHE)
+def determine_ip_origin(ip_address):
+    """ returns country from ip """
+    return gi.country_name_by_addr(ip_address)
 
 
 if __name__ == '__main__':
